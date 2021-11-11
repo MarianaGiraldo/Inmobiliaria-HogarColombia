08ebb1703e5834636efb84640ffa1ac082c6be50
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-11-10 22:27:32 -0500

    Creación de Entidad Cliente y Datasource

---
+++ src/models/cliente.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Cliente extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  departamento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ciudad: string;
+
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
