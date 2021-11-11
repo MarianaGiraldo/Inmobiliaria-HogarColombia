da4d47c51df6b7429ea4d857d7ae53830b668e19
WebCAZ89 <webcaz89@gmail.com>
2021-11-10 22:09:56 -0500

    Creación entidad Imueble

---
+++ src/models/inmueble.model.ts
@@ -0,0 +1,88 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Inmueble extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  asesorId: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  departamento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ciudad: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  valor: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  tipo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  tipoOferta: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  encargado: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  encargadoContacto: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  fotografias: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  videoUrl: string;
+
+
+  constructor(data?: Partial<Inmueble>) {
+    super(data);
+  }
+}
+
+export interface InmuebleRelations {
+  // describe navigational properties here
+}
+
+export type InmuebleWithRelations = Inmueble & InmuebleRelations;
