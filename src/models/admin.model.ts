cfea458312308389f37b364706e28b66700503fa
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-11-23 22:40:58 -0500

    Model Entity Admin

---
+++ src/models/admin.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Admin extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  contrasena: string;
+
+
+  constructor(data?: Partial<Admin>) {
+    super(data);
+  }
+}
+
+export interface AdminRelations {
+  // describe navigational properties here
+}
+
+export type AdminWithRelations = Admin & AdminRelations;
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from './cliente.model';
 export * from './asesor.model';
 export * from './inmueble.model';
 export * from './solicitud.model';
+export * from './admin.model';
