5e3d2bf8976f93c6aa285cc8b94ba7e238d461b1
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-11-23 22:46:54 -0500

    Cliente controller created

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './cliente-solicitud.controller';
 export * from './solicitud-cliente.controller';
 export * from './inmueble-solicitud.controller';
 export * from './solicitud-inmueble.controller';
+export * from './cliente.controller';
